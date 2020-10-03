0c2a25a18751715666091aa789304095321f0cc3
Alex Nagy <therebeloper@gmail.com>
2020-10-03 10:38:37 +0300

    Initial Commit

---
+++ QonversionExample/QonversionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QonversionExampleApp.swift
+//  QonversionExample
+//
+//  Created by Alex Nagy on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct QonversionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
